058ce5faaa42826cc0d90935ee9766dc2b6e1927
Robin hawlader <robinhawlader360@gmail.com>
2021-09-09 19:22:36 +0600

    comment out

--- email.js
+++ email.js
@@ -5,17 +5,10 @@ const emailSubmit = () => {
     if (regex.test(inputValue)) {
         document.getElementById('success').innerText = `valid`;
         document.getElementById('invalid').innerText = '';
-        // const p = document.createElement('p');
-        // p.classList.add('fst-italic', 'text-success');
-        // p.innerText = `your email is valid`;
-        // userMessage.appendChild(p);
     }
     else {
         document.getElementById('success').innerText = ``;
         document.getElementById('invalid').innerText = 'not valid';
-        // p.classList.add('fst-italic', 'text-danger');
-        // p.innerText = `Opps!!! your email is invalid`;
-        // userMessage.appendChild(p);
     }
     inputField.value = '';
 
