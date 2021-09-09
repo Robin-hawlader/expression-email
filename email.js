35ab4de574096948681cbad74cbc737e5ea18387
Robin hawlader <robinhawlader360@gmail.com>
2021-09-09 19:51:44 +0600

    fancy

--- email.js
+++ email.js
@@ -5,9 +5,17 @@ const emailSubmit = () => {
     if (regex.test(inputValue)) {
         document.getElementById('success').innerText = `your email is valid.`;
         document.getElementById('invalid').innerText = '';
+        document.getElementById('empty-input').innerText = '';
+    }
+    else if (inputValue === '') {
+        const emptyInput = document.getElementById('empty-input');
+        emptyInput.innerText = `Please put an email`;
+        document.getElementById('success').innerText = ``;
+        document.getElementById('invalid').innerText = '';
     }
     else {
         document.getElementById('success').innerText = ``;
+        document.getElementById('empty-input').innerText = '';
         document.getElementById('invalid').innerText = 'Opss!! your email is notvalid';
     }
     inputField.value = '';
