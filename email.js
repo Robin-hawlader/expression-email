b94067b4f2f17a760bef2fb4daf5a9ba4aa3e4e7
Robin hawlader <robinhawlader360@gmail.com>
2021-09-09 19:26:59 +0600

    message

--- email.js
+++ email.js
@@ -3,12 +3,12 @@ const emailSubmit = () => {
     const inputValue = inputField.value;
     const regex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
     if (regex.test(inputValue)) {
-        document.getElementById('success').innerText = `valid`;
+        document.getElementById('success').innerText = `your email is valid.`;
         document.getElementById('invalid').innerText = '';
     }
     else {
         document.getElementById('success').innerText = ``;
-        document.getElementById('invalid').innerText = 'not valid';
+        document.getElementById('invalid').innerText = 'Opss!! your email is notvalid';
     }
     inputField.value = '';
 
